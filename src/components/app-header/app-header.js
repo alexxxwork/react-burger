b14088071429a69e3cb2ebc49cce8823420e0e64
A Dv <advoretsky@ya.ru>
2022-10-27 05:07:38 +0500

    Structure commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components' ;
+
+
+function AppHeader() {
+  return (
+    <div className="">
+      <header className="">
+        <Tab />
+      </header>
+    </div>
+  );
+}
+
+export default AppHeader;
---
+++ src/components/main/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppHeader from '../app-header/app-header';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components' ;
+
+
+function Main() {
+  return (
+    <div className="">
+      <AppHeader />
+    </div>
+  );
+}
+
+export default Main;
