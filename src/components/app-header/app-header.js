db464f07baae87e93db7b2ae13d86e917adec735
A Dv <advoretsky@ya.ru>
2022-10-30 20:39:53 +0500

    Fixed header css styles

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -11,33 +11,35 @@ import styles from './app-header.module.css';
 function AppHeader() {
     return (
         <header className={styles.header}>
-            <div className={styles.left_block}>
-                <HeaderElement>
-                    <div className={styles.element}>
-                        <BurgerIcon type="primary" />
-                    </div>
-                    <div className={styles.element}>Конструктор</div>
-                </HeaderElement>
-                <HeaderElement>
-                    <div className={styles.element}>
-                        <ListIcon type="primary" />
-                    </div>
-                    <div className={styles.element}>Лента заказов</div>
-                </HeaderElement>
-            </div>
-            <div className={styles.logo}>
-                <Logo />
-            </div>
-            <div className={styles.right_block}>
-                {/* <HeaderElement>
+            <div className={styles.header_container}>
+                <div className={styles.left_block}>
+                    <HeaderElement>
+                        <div className={styles.element}>
+                            <BurgerIcon type="primary" />
+                        </div>
+                        <div className={styles.element}>Конструктор</div>
+                    </HeaderElement>
+                    <HeaderElement>
+                        <div className={styles.element}>
+                            <ListIcon type="primary" />
+                        </div>
+                        <div className={styles.element}>Лента заказов</div>
+                    </HeaderElement>
+                </div>
+                <div className={styles.logo}>
+                    <Logo />
+                </div>
+                <div className={styles.right_block}>
+                    {/* <HeaderElement>
                     <div className={styles.element} />
                 </HeaderElement> */}
-                <HeaderElement>
-                    <div className={styles.element}>
-                        <ProfileIcon type="primary" />
-                    </div>
-                    <div className={styles.element}>Личный кабинет</div>
-                </HeaderElement>
+                    <HeaderElement>
+                        <div className={styles.element}>
+                            <ProfileIcon type="primary" />
+                        </div>
+                        <div className={styles.element}>Личный кабинет</div>
+                    </HeaderElement>
+                </div>
             </div>
         </header>
     );
